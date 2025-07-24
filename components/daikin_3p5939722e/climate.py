02fd1d65f5373404612b0028764b455c866211af
Danijel Tudek <4267773-danijelt@users.noreply.gitlab.com>
2025-07-24 19:11:06 +0200

    update component to use climate_schema

--- components/daikin_3p5939722e/climate.py
+++ components/daikin_3p5939722e/climate.py
@@ -1,20 +1,13 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 
-daikin_ns = cg.esphome_ns.namespace("daikin_3p5939722e")
-Dakin3p5939722eClimate = daikin_ns.class_("Dakin3p5939722eClimate", climate_ir.ClimateIR)
+daikin_3p5939722e_ns = cg.esphome_ns.namespace("daikin_3p5939722e")
+Dakin3p5939722eClimate = daikin_3p5939722e_ns.class_("Dakin3p5939722eClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(Dakin3p5939722eClimate),
-    }
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(Dakin3p5939722eClimate)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
+    await climate_ir.new_climate_ir(config)
